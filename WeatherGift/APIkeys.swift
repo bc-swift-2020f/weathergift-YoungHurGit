cbfec1bf4a85ab59e1d161a9b151b251c7a394e2
Young Hur <huryo@bc.edu>
2020-10-06 12:33:42 -0400

    Finished 6.4 - Create + Modify Podfile + Hiding APIkey

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Young Hur on 10/6/20.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyABe37TgPz_7doZF1weqw23dVBC1Sm1gHg"
+}
