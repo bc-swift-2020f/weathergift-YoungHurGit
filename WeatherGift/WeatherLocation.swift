7a26638d08f94437bbabfd240abe43938209958a
Young Hur <huryo@bc.edu>
2020-10-05 20:14:39 -0400

    Finished 6.2 - WeatherLocations List Table View

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Young Hur on 10/5/20.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
