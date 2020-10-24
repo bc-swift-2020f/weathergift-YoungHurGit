d2faa43f35055dd338aeca692fa3ae1f2a99642e
Young Hur <huryo@bc.edu>
2020-10-24 18:27:27 -0400

    Finished 6.17 - Custom CollectionView Cells for Displaying Hourly Weather Forecast Data

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -21,6 +21,7 @@ class LocationDetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var weatherDetail: WeatherDetail!
     var locationIndex = 0
@@ -31,6 +32,8 @@ class LocationDetailViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         updateUserInterface()
     }
@@ -61,6 +64,7 @@ class LocationDetailViewController: UIViewController {
                 self.summaryLabel.text = self.weatherDetail.summary
                 self.imageView.image = UIImage(named: self.weatherDetail.dayIcon)
                 self.tableView.reloadData()
+                self.collectionView.reloadData()
             }
         }
     }
@@ -89,7 +93,6 @@ class LocationDetailViewController: UIViewController {
         }
         
         pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: sender.currentPage)], direction: direction, animated: true, completion: nil)
-
     }
     
 }
@@ -107,6 +110,19 @@ extension LocationDetailViewController: UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
+    }
+    
+}
+
+extension LocationDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return weatherDetail.hourlyWeatherData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let hourlyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HourlyCell", for: indexPath) as! HourlyCollectionViewCell
+        hourlyCell.hourlyWeather = weatherDetail.hourlyWeatherData[indexPath.row]
+        return hourlyCell
     }
     
 }
