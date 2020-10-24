9e1306a0c5158a5d98b0ca4690546792adcc64be
Young Hur <huryo@bc.edu>
2020-10-24 16:42:23 -0400

    Finished 6.15 - Daily Weather in Custom TableView Cells

---
+++ WeatherGift/DailyTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DailyTableViewCell.swift
+//  WeatherGift
+//
+//  Created by Young Hur on 10/24/20.
+//
+
+import UIKit
+
+class DailyTableViewCell: UITableViewCell {
+    @IBOutlet weak var dailyImageView: UIImageView!
+    @IBOutlet weak var dailyWeekdayLabel: UILabel!
+    @IBOutlet weak var dailyHighLabel: UILabel!
+    @IBOutlet weak var dailyLowLabel: UILabel!
+    @IBOutlet weak var dailySummaryView: UITextView!
+    
+    var dailyWeather: DailyWeather! {
+        didSet {
+            dailyImageView.image = UIImage(named: dailyWeather.dailyIcon)
+            dailyWeekdayLabel.text = dailyWeather.dailyWeekday
+            dailySummaryView.text = dailyWeather.dailySummary
+            dailyHighLabel.text = "\(dailyWeather.dailyHigh)°"
+            dailyLowLabel.text = "\(dailyWeather.dailyLow)°"
+        }
+    }
+}
